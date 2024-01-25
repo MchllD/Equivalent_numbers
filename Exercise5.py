f706cf2a0bc7410ca1f61ba9d59e4f7e6df7455f
MchllD <mich11dimafelix@gmail.com>
2024-01-26 00:32:42 +0800

    pseudocode

---
+++ Exercise5.py
@@ -0,0 +1,15 @@
+# Exercise 5: Check if the first and last number of a list is the same
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+
+# pseudocode
+
+# Function to check the similarity of the first and last numbers in a list
+# Get the first and last numbers from the list
+# Check if the first and last numbers are the same
+# Return True to indicate successful match
+# Return False to indicate no match
+# Sample 1
+# Sample 2
+# Sample 3
+# Call the check_first_last_similarity function with the first, second & third sample
